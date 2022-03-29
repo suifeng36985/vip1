a70f522952600c93d178bafac9507beef5712867
suifeng36985 <52465048+suifeng36985@users.noreply.github.com>
2022-03-30 04:19:16 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "95E89714-1AE6-9124-26F6-EBB9B900EF58",
                         "alterId": 0
                     }
                 ],
